d00fcfc95074915b1085b6d212c58a8cc3d82540
Noah Cantor <noah.cantor@forward.co.uk>
2011-05-25 11:40:07 +0100

    Added better handling for deployments without proper instance names

--- lib/capify-ec2.rb
+++ lib/capify-ec2.rb
@@ -52,27 +52,29 @@ class CapifyEc2
   def self.deregister_instance_from_elb(instance_name)
     return unless @ec2_config[:load_balanced]
     @instance = get_instance_by_name(instance_name).first
+    return if @instance.nil?
     @elb_name = get_elb_name_by_instance(@instance.id)
     @elb.deregister_instances_from_load_balancer(@instance.id, @elb_name) unless @elb_name.nil?
   end
   
-    def self.register_instance_in_elb
-      return unless @ec2_config[:load_balanced]
-      fail_after = !@ec2_config[:fail_after].nil? ? @ec2_config[:fail_after] : 30
-      @elb.register_instances_with_load_balancer(@instance.id, @elb_name) unless @elb_name.nil?
+  def self.register_instance_in_elb
+    return unless @ec2_config[:load_balanced]
+    return if (@instance.nil? || @elb_name.nil?)
+    fail_after = !@ec2_config[:fail_after].nil? ? @ec2_config[:fail_after] : 30
+    @elb.register_instances_with_load_balancer(@instance.id, @elb_name) unless @elb_name.nil?
+    state = @elb.describe_instance_health(@elb_name, @instance.id).body['DescribeInstanceHealthResult']['InstanceStates'][0]['State']
+    time_elapsed = 0
+    sleepcount = 5
+    until (state == 'InService' || time_elapsed >= fail_after)
+      sleep sleepcount
+      time_elapsed += sleepcount
+      puts 'Verifying Instance Health'
       state = @elb.describe_instance_health(@elb_name, @instance.id).body['DescribeInstanceHealthResult']['InstanceStates'][0]['State']
-      time_elapsed = 0
-      sleepcount = 5
-      until (state == 'InService' || time_elapsed >= fail_after)
-        sleep sleepcount
-        time_elapsed += sleepcount
-        puts 'Verifying Instance Health'
-        state = @elb.describe_instance_health(@elb_name, @instance.id).body['DescribeInstanceHealthResult']['InstanceStates'][0]['State']
-      end
-      if state == 'InService'
-        puts "#{@instance.tags['Name']}: Healthy"
-      else
-        puts "#{@instance.tags['Name']}: tests timed out after #{time_elapsed} seconds."
-      end
+    end
+    if state == 'InService'
+      puts "#{@instance.tags['Name']}: Healthy"
+    else
+      puts "#{@instance.tags['Name']}: tests timed out after #{time_elapsed} seconds."
     end
   end
+end
--- lib/capify-ec2/version.rb
+++ lib/capify-ec2/version.rb
@@ -1,5 +1,5 @@
 module Capify
   module Ec2
-    VERSION = "1.1.2"
+    VERSION = "1.1.3"
   end
 end
