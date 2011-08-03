3351fb80b2203e82cbf0fd9e9fbf8beea2cdacc8
Noah Cantor <noah.cantor@forward.co.uk>
2011-08-03 12:06:45 +0100

    Fog bug. instance.name and instance.case_insensitive_tag("Name") aren not always the same.

--- lib/capify-ec2/capistrano.rb
+++ lib/capify-ec2/capistrano.rb
@@ -99,7 +99,7 @@ Capistrano::Configuration.instance(:must_exist).load do
   task :ec2_status do
     CapifyEc2.running_instances.each_with_index do |instance, i|
       puts sprintf "%-11s:   %-40s %-20s %-20s %-62s %-20s (%s)",
-        i.to_s.magenta, instance.name, instance.id.red, instance.flavor_id.cyan,
+        i.to_s.magenta, instance.case_insensitive_tag("Name"), instance.id.red, instance.flavor_id.cyan,
         instance.dns_name.blue, instance.availability_zone.green, instance.roles.join(", ").yellow
     end
   end
--- lib/capify-ec2/version.rb
+++ lib/capify-ec2/version.rb
@@ -1,5 +1,5 @@
 module Capify
   module Ec2
-    VERSION = "1.1.9"
+    VERSION = "1.1.10"
   end
 end
