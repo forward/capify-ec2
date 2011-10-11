6a0c512f034b171ee718159679e7cdeec01ef708
Noah Cantor <noah.cantor@forward.co.uk>
2011-10-11 13:52:20 +0100

    Revert "Added first stage of using elastic IPs."
    This reverts commit f01227f978affb4338279c9369603620cf6402e3.

--- lib/capify-ec2/capistrano.rb
+++ lib/capify-ec2/capistrano.rb
@@ -42,11 +42,6 @@ Capistrano::Configuration.instance(:must_exist).load do
       puts "Running `#{command}`"
       exec(command)
     end
-    
-    desc "Allocate Elastic IP address to AWS account"
-    task :allocate_address do
-      CapifyEc2.allocate_address
-    end
 
   end
   
@@ -59,7 +54,6 @@ Capistrano::Configuration.instance(:must_exist).load do
   def ec2_roles(*roles)
     server_name = variables[:logger].instance_variable_get("@options")[:actions].first unless variables[:logger].instance_variable_get("@options")[:actions][1].nil?
     named_instance = CapifyEc2.get_instance_by_name(server_name)
-    
     task named_instance.name.to_sym do
       remove_default_roles
       server_address = named_instance.dns_name
