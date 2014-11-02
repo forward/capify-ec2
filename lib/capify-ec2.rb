0aa1f0b546fedc4b9ede8023ac47e0978dab7033
Stuart Quin <stuart.quin@gmail.com>
2014-11-02 20:20:09 +0000

    Move cloudwatch stuff out

--- lib/capify-ec2.rb
+++ lib/capify-ec2.rb
@@ -7,18 +7,8 @@ require 'colored'
 require 'net/http'
 require 'net/https'
 require File.expand_path(File.dirname(__FILE__) + '/capify-ec2/server')
+require File.expand_path(File.dirname(__FILE__) + '/capify-ec2/cloudwatch')
 
-
-def get_spark values
-  ticks = %w[▁ ▂ ▃ ▄ ▅ ▆ ▇]
-  min, range, scale = values.min, 100 - values.min, ticks.length - 1
-  if !(range == 0)
-    bar = values.map { |x| ticks[(x / 100.0 * scale).floor] }.join
-    return bar + " #{values.last.floor}%"
-  else
-    return values.map { |x| ticks[1] }.join
-  end
-end
 
 class CapifyEc2
 
@@ -92,39 +82,13 @@ class CapifyEc2
     @ec2_config[:aws_secret_access_key] || Fog.credentials[:aws_secret_access_key] || ENV['AWS_SECRET_ACCESS_KEY'] || @ec2_config[:use_iam_profile] || raise("Missing AWS Secret Access Key")
   end
 
-  def fetch_cpu instance_id
-    @cw = Fog::AWS::CloudWatch.new(:aws_access_key_id => aws_access_key_id,
-                                   :aws_secret_access_key => aws_secret_access_key)
-                                   
-    time = Time.new
-    time.gmtime
-
-    dimensions = [{
-      "Name" => "InstanceId",
-      "Value" => instance_id
-    }]
-
-    result = @cw.get_metric_statistics({'Namespace' => 'AWS/EC2',
-                                        'MetricName' => 'CPUUtilization',
-                                        'Period' => 120,
-                                        'Statistics' => ['Average'],
-                                        'StartTime' => DateTime.parse((time - 60*60).to_s),
-                                        'EndTime' => DateTime.parse(time.to_s),
-                                        'Dimensions' => dimensions})
-
-    datapoints = result.body.fetch("GetMetricStatisticsResult", {})["Datapoints"]
-
-    if datapoints
-      return get_spark datapoints.map {|x| x["Average"]}
-    end
-    return ""
-  end
-
   def display_instances
     unless desired_instances and desired_instances.any?
       puts "[Capify-EC2] No instances were found using your 'ec2.yml' configuration.".red.bold
       return
     end
+
+    cw = CapifyCloudwatch.new(aws_access_key_id, aws_secret_access_key)
 
     # Set minimum widths for the variable length instance attributes.
     column_widths = { :name_min => 4, :type_min => 4, :dns_min => 5, :roles_min => @ec2_config[:aws_roles_tag].length, :stages_min => @ec2_config[:aws_stages_tag].length, :options_min => @ec2_config[:aws_options_tag].length }
@@ -168,7 +132,7 @@ class CapifyEc2
       status_output << instance.flavor_id                                  .ljust( column_widths[:type]    ).cyan
       status_output << instance.contact_point                              .ljust( column_widths[:dns]     ).blue.bold
       status_output << instance.availability_zone                          .ljust( 10                      ).magenta
-      status_output << fetch_cpu(instance.id).ljust( 16                      ).green
+      status_output << cw.get_metric(instance.id, "CPUUtilization").ljust(16).green
       status_output << (instance.tags[@ec2_config[:aws_stages_tag]]  || '').ljust( column_widths[:stages]  ).yellow if stages_present
       status_output << (instance.tags[@ec2_config[:aws_roles_tag]]   || '').ljust( column_widths[:roles]   ).yellow if roles_present
       status_output << (instance.tags[@ec2_config[:aws_options_tag]] || '').ljust( column_widths[:options] ).yellow if options_present
---
+++ lib/capify-ec2/cloudwatch.rb
@@ -0,0 +1,48 @@
+#!/usr/bin/env ruby
+# -*- coding: utf-8 -*-
+
+require 'fog'
+
+class CapifyCloudwatch
+  def initialize(key_id, secret)
+    @ticks = %w[▁ ▂ ▃ ▄ ▅ ▆ ▇]
+    @cw = Fog::AWS::CloudWatch.new(:aws_access_key_id => key_id,
+                                   :aws_secret_access_key => secret)
+  end
+
+  def get_metric(instance_id, metric, hours = 1)
+    range = hours * (60 * 60)
+    time = Time.new
+    start = DateTime.parse((time - range).to_s)
+    finish = DateTime.parse(time.to_s)
+
+    dimensions = [{
+      "Name" => "InstanceId",
+      "Value" => instance_id
+    }]
+
+    result = @cw.get_metric_statistics({'Namespace' => 'AWS/EC2',
+                                        'MetricName' => metric,
+                                        'Period' => 120,
+                                        'Statistics' => ['Average'],
+                                        'StartTime' => start,
+                                        'EndTime' => finish,
+                                        'Dimensions' => dimensions})
+
+    dp = result.body.fetch("GetMetricStatisticsResult", {})["Datapoints"]
+
+    require "pry"
+    binding.pry
+
+    if dp
+      return get_spark_line(dp.map {|x| x["Average"]})
+    end
+    return ""
+  end
+
+  def get_spark_line values
+    scale = @ticks.length - 1
+    bar = values.map { |x| @ticks[(x / 100.0 * scale).floor] }.join
+    return bar + " #{values.last.floor}%"
+  end
+end
